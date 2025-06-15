96918a82b947587cd1d17935ce0b05aea4d70540
雅斗 <maboo1114@gmail.com>
2025-06-15 16:02:42 +0900

    changes:App.vue 変更

---
+++ vue-lesson/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+<template>
+  <p>{{ count }}</p>
+  <button v-on:click="count++">button</button>
+</template>
+<style></style>
--- vue-lesson/src/main.js
+++ vue-lesson/src/main.js
@@ -1,2 +1,4 @@
 import { createApp } from 'vue'
-createApp()
+import App from './App.vue'
+const app = createApp(App)
+app.mount('#app')
